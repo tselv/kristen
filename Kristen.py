3cdc88fc99adc987777ccb91038c7ef88dd48321
Tharun <tharun@0587374046.wireless.umich.net>
2017-03-22 16:31:27 -0400

    Recording mostly works

--- Kristen.py
+++ Kristen.py
@@ -9,6 +9,16 @@ with open('secrets.txt') as f:
 
 client = discord.Client()
 
+def has_role(roles_list, role_name):
+    for role in roles_list:
+        if role.name == role_name:
+            return True
+
+    return False
+
+def make_name(message):
+    return "__**" + (message.author.nick if message.author.nick else message.author.name) + " said:**__\n"
+
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -19,8 +29,20 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('!test'):
-        await client.send_message(message.channel, 'Test response')
+    if message.author.bot == False and has_role(message.author.roles, "Recorded"):
+        if len(client.messages) > 1:
+            previous_message = client.messages[len(client.messages)-2]
+        if len(client.messages) > 1 and previous_message.author.id == client.user.id and previous_message.content.startswith(make_name(message)):
+            update = previous_message.content[:-3] + message.content + "\n```\n"
+            await client.edit_message(previous_message, update)
+        else:
+            recording = make_name(message)
+            recording += "```\n"
+            recording += message.content
+            recording += "\n```\n"
+            await client.send_message(message.channel, recording)
+        await client.delete_message(message)
+
 
 
 client.run(token)
